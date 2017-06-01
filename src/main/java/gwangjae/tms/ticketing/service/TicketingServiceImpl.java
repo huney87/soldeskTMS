d6ab70b4f8841e10d5416dd7d3690b46f88c9032
edga <yoo5459@gmail.com>
2017-06-01 17:05:37 +0900

    일정 통신 준비

--- src/main/java/gwangjae/tms/ticketing/service/TicketingServiceImpl.java
+++ src/main/java/gwangjae/tms/ticketing/service/TicketingServiceImpl.java
@@ -1,12 +1,17 @@
 package gwangjae.tms.ticketing.service;
 
+import java.sql.Date;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class TicketingServiceImpl implements TicketingService {
-
+	/*
+	 * 좌석 레이아웃 2차원 배열 출력
+	 * @see gwangjae.tms.ticketing.service.TicketingService#drawLayout()
+	 */
 	@Override
-	public String[][] drawLayout() {
+	public String[][] drawLayout(int hall_id) {
 		String[][] layout = new String[10][];
 		for(int i=0;i < layout.length;i++){
 			layout[i] = new String[20];
@@ -15,6 +20,16 @@ public class TicketingServiceImpl implements TicketingService {
 			}
 		}
 		return layout;
+	}
+
+	/*
+	 * 공연 일정을 Date 배열로 출력
+	 * @see gwangjae.tms.ticketing.service.TicketingService#getPerfSkd()
+	 */
+	@Override
+	public Date[] getPerfSkd(int perf_id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
