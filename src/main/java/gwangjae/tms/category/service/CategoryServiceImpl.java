1e8c1de2c54c3f4c2d72487d8cb831be1b37601b
DESKTOP-FHG2IGG\wonseo <pdords@naver.com>
2017-06-08 16:20:46 +0900

    박원서 sql구문 장르구분, 지역구분 테스트 성공 날짜구분, 통합검색,순위별남음

--- src/main/java/gwangjae/tms/category/service/CategoryServiceImpl.java
+++ src/main/java/gwangjae/tms/category/service/CategoryServiceImpl.java
@@ -1,6 +1,9 @@
 package gwangjae.tms.category.service;
 
 import gwangjae.tms.category.dao.CategoryDao;
+import gwangjae.tms.category.domain.Category;
+import gwangjae.tms.hall.domain.Center;
+import gwangjae.tms.hall.domain.Hall;
 import gwangjae.tms.performance.domain.Performance;
 
 import java.util.List;
@@ -14,7 +17,13 @@ public class CategoryServiceImpl implements CategoryService{
 	
 	@Override
 	public List<Performance> searchPerfomances(){
+		Category cate = new Category();
+		Performance per = new Performance();
+		Hall hall = new Hall();
+		Center cen = new Center();
+		if(cate.getGen_id() != 0){
+			dao.getSearchResult();
+		}
 		return dao.getSearchResult();
 	}
-
 }
